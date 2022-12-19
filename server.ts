4243bb004a287f8e8bf9eb97fe09e4fdad878bad
kota <52403688+yashikota@users.noreply.github.com>
2022-12-20 07:01:36 +0900

    finish

--- server.ts
+++ server.ts
@@ -1,19 +1,16 @@
+import { getLatestAbc } from "./scraper.ts";
 import { serve } from "https://deno.land/std@0.154.0/http/server.ts";
 const PORT = 8080;
 
 const handler = async (req: Request) => {
-  const url = "https://kenkoooo.com/atcoder/resources/contests.json";
-  const json = await fetch(url).then((res) => res.json());
-  const latestAbc = json.filter((contest: any) => contest.id.startsWith("abc"))
-    .sort((a: any, b: any) => b.start_epoch_second - a.start_epoch_second)[0];
-  const start = new Date(latestAbc.start_epoch_second * 1000);
+  const latestAbc: any = await getLatestAbc();
 
   if (req.method === "GET") {
     return new Response(
       JSON.stringify({
-        title: latestAbc.title,
-        start: start.toLocaleString("ja-JP", { timeZone: "Asia/Tokyo" }),
-        url: `https://atcoder.jp/contests/${latestAbc.id}`,
+        start: latestAbc[0],
+        title: latestAbc[1],
+        url: latestAbc[2],
       }),
       {
         headers: {
