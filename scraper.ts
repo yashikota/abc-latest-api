75c98e92b2fb8e72d7c55193fe028662437ca107
kota <52403688+yashikota@users.noreply.github.com>
2023-02-25 18:12:38 +0900

    fmt

--- scraper.ts
+++ scraper.ts
@@ -1,42 +1,42 @@
 import { DOMParser } from "https://deno.land/x/deno_dom@v0.1.36-alpha/deno-dom-wasm.ts";
 
 export const getLatestAbc = async () => {
-  const url = "https://atcoder.jp";
+    const url = "https://atcoder.jp";
 
-  try {
-    const res = await fetch(url + "/?lang=ja");
-    const html = await res.text();
-    const doc: any = new DOMParser().parseFromString(html, "text/html");
+    try {
+        const res = await fetch(url + "/?lang=ja");
+        const html = await res.text();
+        const doc: any = new DOMParser().parseFromString(html, "text/html");
 
-    // DOMを取得
-    const times = doc.querySelectorAll(".m-list_contest_info .time");
-    const titles = doc.querySelectorAll(".m-list_contest_ttl");
-    const links = doc.querySelectorAll(".m-list_contest_ttl a");
+        // DOMを取得
+        const times = doc.querySelectorAll(".m-list_contest_info .time");
+        const titles = doc.querySelectorAll(".m-list_contest_ttl");
+        const links = doc.querySelectorAll(".m-list_contest_ttl a");
 
-    // timesの表記が「2022-12-17 21:00:00+0900」のようになっているので、JSTに変換
-    for (let i = 0; i < times.length; i++) {
-      const time = times[i].textContent.replace("開始", "");
-      const date = new Date(time);
-      const jst = date.toLocaleString("ja-JP", { timeZone: "Asia/Tokyo" });
-      times[i].textContent = jst;
+        // timesの表記が「2022-12-17 21:00:00+0900」のようになっているので、JSTに変換
+        for (let i = 0; i < times.length; i++) {
+            const time = times[i].textContent.replace("開始", "");
+            const date = new Date(time);
+            const jst = date.toLocaleString("ja-JP", { timeZone: "Asia/Tokyo" });
+            times[i].textContent = jst;
+        }
+
+        // 2次元配列として格納
+        const contestsList = [];
+        for (let i = 0; i < times.length; i++) {
+            const time = times[i].textContent.replace(/[\n\t\r]/g, "");
+            const title = titles[i].textContent.replace(/[\n\t\r]/g, "");
+            const link = url + links[i].getAttribute("href");
+            contestsList.push([time, title, link]);
+        }
+
+        // Beginnerが含まれるコンテストのみを抽出し、開催時刻が最新のものだけを取得
+        const latestAbc = (contestsList.filter((contest) =>
+            contest[1].includes("Beginner")
+        ))[0];
+
+        return latestAbc;
+    } catch (error) {
+        console.log(error);
     }
-
-    // 2次元配列として格納
-    const contestsList = [];
-    for (let i = 0; i < times.length; i++) {
-      const time = times[i].textContent.replace(/[\n\t\r]/g, "");
-      const title = titles[i].textContent.replace(/[\n\t\r]/g, "");
-      const link = url + links[i].getAttribute("href");
-      contestsList.push([time, title, link]);
-    }
-
-    // Beginnerが含まれるコンテストのみを抽出し、開催時刻が最新のものだけを取得
-    const latestAbc = (contestsList.filter((contest) =>
-      contest[1].includes("Beginner")
-    ))[0];
-
-    return latestAbc;
-  } catch (error) {
-    console.log(error);
-  }
 };
--- server.ts
+++ server.ts
@@ -3,26 +3,26 @@ import { serve } from "https://deno.land/std@0.154.0/http/server.ts";
 const PORT = 8080;
 
 const handler = async (req: Request) => {
-  const latestAbc: any = await getLatestAbc();
+    const latestAbc: any = await getLatestAbc();
 
-  if (req.method === "GET") {
-    return new Response(
-      JSON.stringify({
-        start: latestAbc[0],
-        title: latestAbc[1],
-        url: latestAbc[2],
-      }),
-      {
-        headers: {
-          "content-type": "application/json",
-        },
-      },
-    );
-  } else {
-    return new Response("Not Found", {
-      status: 404,
-    });
-  }
+    if (req.method === "GET") {
+        return new Response(
+            JSON.stringify({
+                start: latestAbc[0],
+                title: latestAbc[1],
+                url: latestAbc[2],
+            }),
+            {
+                headers: {
+                    "content-type": "application/json",
+                },
+            },
+        );
+    } else {
+        return new Response("Not Found", {
+            status: 404,
+        });
+    }
 };
 
 await serve(handler, { port: PORT });
